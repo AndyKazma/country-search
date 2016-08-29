02159b01fd429eec58731ae2f6f47077ce11674e
AndyKazma <slavek@123321.pl>
2016-08-29 23:16:00 +0200

    Error handling added

--- script.js
+++ script.js
@@ -6,19 +6,31 @@ $('#search').click(searchCountries);
 
 function searchCountries() {
  	var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
-$.ajax({
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
   		url: url + countryName,
   		method: 'GET',
-  		success: showCountriesList
+  		success: showCountriesList,
+  		error: function (jqXHR, status, err) {
+    		alert("Error: Data not found");
+  			}
   	});
 }
 
 function showCountriesList(resp) {
   	countriesList.empty();
-resp.forEach(function(item){
-   	$('<li>').text(item.name).appendTo(countriesList);
-
-   	$('<li>').text(item.capital).appendTo(countriesList);
-});
+	resp.forEach(function(item){
+	   	$('<li>').text(item.name + " / " +item.nativeName).appendTo(countriesList);
+	});
 }
+
+//  	error: showErrorMsg
+/* function showErrorMsg(jqxhr, textStatus, errorThrown)  {
+        if(jqxhr.status == 404) {
+            alert("page not found!");
+        }
+    }
+
+function showErrorMsg(jqXHR, textStatus, errorThrown){
+	alert("Error: " + textStatus + " : " + errorThrown) ;
+}*/
